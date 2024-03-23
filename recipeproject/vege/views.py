f124cd0173ff2a678ab7cfc66fcb30b3ac11743f
haseeb4339 <haseebullahbj@gmail.com>
2024-03-23 18:05:55 +0500

    create a register view with error messages

--- recipeproject/vege/views.py
+++ recipeproject/vege/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import Recipe
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+
 
 
 def Recipes(request):
@@ -79,4 +83,35 @@ def login_page(request):
 
 
 def register(request):
+
+    if request.method == "POST":
+
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+
+            messages.error(request, "Username already exists.")
+
+            return redirect('/register/')
+
+        user = User.objects.create(
+            first_name = first_name,
+            last_name = last_name,
+            username = username
+        )
+        user.set_password(password)
+
+        user.save()
+
+        messages.success(request, "Account created successfully.")
+
+        return redirect('/login/')
+
+
+
     return render(request, 'register.html')
