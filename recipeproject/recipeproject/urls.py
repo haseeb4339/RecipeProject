3ca01ebb6f49a645c922e1e0e8e8bdeb89def48c
haseeb4339 <haseebullahbj@gmail.com>
2024-03-23 16:10:43 +0500

    create a update functionality

--- recipeproject/recipeproject/urls.py
+++ recipeproject/recipeproject/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from vege.views import Recipes, Delete_Recipe
+from vege.views import Recipes, Delete_Recipe, Update_Recipe
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -25,6 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('recipes/', Recipes, name='recipes'),
     path('delete_recipe/<id>/', Delete_Recipe, name='delete_recipe'),
+    path('update_recipe/<id>/', Update_Recipe, name='update_recipe'),
 ]
 
 if settings.DEBUG:
--- recipeproject/vege/views.py
+++ recipeproject/vege/views.py
@@ -29,6 +29,38 @@ def Recipes(request):
     return render(request, 'recipes.html', context)
 
 
+
+def Update_Recipe(request, id):
+    
+    queryset = Recipe.objects.get(id=id)
+
+    if request.method == "POST":
+
+        data = request.POST
+        
+        recipe_name = data.get('recipe_name')
+        recipe_description = data.get('recipe_description')
+        recipe_image = request.FILES.get('recipe_image')
+
+        queryset.recipe_name = recipe_name
+        queryset.recipe_description = recipe_description
+
+        if recipe_image:
+            queryset.recipe_image = recipe_image
+
+
+        queryset.save()
+
+        return redirect('/recipes/')
+
+        
+
+
+    context = {'Recipe': queryset}
+
+    return render(request, 'update.html', context)
+
+
 def Delete_Recipe(request, id):
 
     queryset = Recipe.objects.get(id=id)
